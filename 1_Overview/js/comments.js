14bda7844430b2440ce1688e48b0c27df8d9895c
emiliobg1997 <emilio.barrera1997@gmail.com>
2020-06-16 17:34:02 -0500

    This is the first commit. It contains the first unit of the course.

---
+++ 1_Overview/js/comments.js
@@ -0,0 +1,5 @@
+//I'm a single line Comment
+/*And i'm a
+multiline
+comment :D
+*/
---
+++ 1_Overview/js/demo.js
@@ -0,0 +1,5 @@
+//Everything here is going to be used in the externalJS.html file to showcase the
+//ability to use external JS
+alert("Hey, just \"alerting\" that i'm an alert from an external js");
+document.write("<h1>This is a title written with JS</h1>");
+document.write("<p>This is a paragraph also written with JS</p>");
